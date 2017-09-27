ffa3676b1c6df313accfbe78f7f7855ad879958b
Charles <cweng@stuy.edu>
2017-09-27 16:39:14 -0400

    some fixes to code

--- app.py
+++ app.py
@@ -4,53 +4,62 @@
 # 2017-09-26
 
 from flask import Flask, render_template
-from decimal import Decimal
 import random
+#from decimal import Decimal for bash method
 
 app = Flask(__name__)
 
-#creates a list of lists with two values to pass to HTML template for table generation
-#the first place stores the job name; the second the percentage
-#maintains original order of list w/o dictionary
-jobList = []
-f = open("occupations.csv", "rU")
-lines = f.read().splitlines()
-i = 0
-while (i < len(lines)):
-    #split each line read by the first rightmost comma (some job names have commas in them)
-    line = lines[i].rsplit(",", 1)
-    jobList.append(line)
-    i += 1
+# returns a list of lists with two values
+## the first place stores the job name; the second the percentage
+## maintains original order of list w/o dictionary
+## includes header + total
+def getJobs(fileName):
+    jobList = []
+    f = open(fileName, "rU")
+    lines = f.read().splitlines()
+    i = 0
+    while (i < len(lines)):
+        #split each line read by the first rightmost comma (some job names have commas in them)
+        line = lines[i].rsplit(",", 1)
+        jobList.append(line)
+        i += 1
+    return jobList
 
-#use our last hw to get a random job
-'''brute force random occupation
-myList = []
-#goes through each list, multiplying each percentage by 10
-for job in jobList:
-    if not (job[0] == "Job Class" or job[0] == "Total"):
-        val = Decimal(job[1]) * 10
-        for x in range(val):
-            myList.append(job[0]) #adds occupation by value
-job = random.choice(myList) #randomly chooses occupation from list
+# load the table into memory since it doesn't change
+jobs = getJobs("occupations.csv")
+
+
+# use our last hw to get a random job
+''' brute force random occupation
+def randomJob():
+    myList = []
+    #goes through each list, multiplying each percentage by 10
+    for job in jobList:
+        if not (job[0] == "Job Class" or job[0] == "Total"):
+            val = Decimal(job[1]) * 10
+            for x in range(val):
+                myList.append(job[0]) #adds occupation by value
+    job = random.choice(myList) #randomly chooses occupation from list
 '''
-#less memory intensive way
-ramdom = random.randrange(len(jobList) - 1)
-i = 1
-while (ramdom > 0):
-    ramdom -= float(jobList[i][1])
-    i += 1
-job = jobList[i]
 
-#Quality of life
+# less memory intensive way
+def randomJob(jobList):
+    ramdom = random.randrange(len(jobList) - 1)
+    i = 1
+    while (ramdom > 0):
+        ramdom -= float(jobList[i][1])
+        i += 1
+    job = jobList[i]
+
+# Quality of life
 @app.route("/")
 def index():
     return "<html>Look at occupations <a href='/occupations'>here</a>.</html>"
 
-#Page that has the goods
-##note: Job does not randomize on reload, but on server start
+# Page that has the goods
 @app.route("/occupations")
 def template():
-    return render_template("template.html", foo = "Random Job", collection = jobList, randJob = job)
+    return render_template("template.html", title = "Random Job", table = jobs, randJob = randomJob(jobs))
 
 
 if __name__ == "__main__":
