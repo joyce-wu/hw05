167f0194ad08ec7367ebe222efab4c23d6ebe70f
Charles <cweng@stuy.edu>
2017-09-26 23:50:43 -0400

    I tried to do some work

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+#Charles Weng and Joyce Wu
+#P7 SoftDev
+#HW05 ... And Now Enjoy Its Contents
+#9/27/18
+
+import  random
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -10,12 +16,26 @@ i = 0
 while (i < len(lines)):
     #split each line read by the first rightmost comma (some job names have commas in them)
     line = lines[i].rsplit(",", 1)
-    jobList[i] = line
+    jobList.append(line)
     i += 1
 
-#use our last hw to get a random job
-job =
+#Get a random job
+ramdom = random.randrange(len(jobList) - 1)
+i = 1
+while (ramdom > 0):
+    ramdom -= float(jobList[i][1])
+    i += 1
+job = jobList[i]
 
 @app.route("/occupations")
 def template():
     return app.render_template("template.html", foo = "Random Job", collection = jobList, randJob = job)
+
+#quality of life
+@app.route('/')
+def hello():
+	return "<a href= '/occupations'>go here</a>."
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
